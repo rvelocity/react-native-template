173a8b003e962075270f0494cf8e1613b9da8018
AdityaTarale <tarale.adi@gmail.com>
2024-10-22 12:18:13 +0530

    Tab stack file setup

---
+++ src/navigation/BottomTabsNavigator.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const BottomTabsNavigator = () => {
+  return (
+    <View>
+      <Text>BottomTabsNavigator</Text>
+    </View>
+  );
+};
+
+export default BottomTabsNavigator;
